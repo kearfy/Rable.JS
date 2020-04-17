efd186b1687f5bbd8ba47f32aa06e74c72c79692
Micha de Vries <contact@michadevries.nl>
2020-04-17 21:53:42 +0200

    Added proper documentation and added static routes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = new Rable({
 	port: 3001
 });
 
+var count = 0;
+
 app.setProxy({
 	host: 'rable.app.michadevries.nl',
 	secure: true
@@ -12,3 +14,4 @@ app.info.set('name', 'Rable.JS Introduction');
 
 app.get('/', __dirname + '/views/home.html');
 app.get('/other', (req, res) => res.sendFile(__dirname + '/views/other.html'));
+app.static('/docs', __dirname + '/docs', {noTemplate: true});
