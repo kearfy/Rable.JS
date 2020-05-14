baf6cb9f6f78d3442028e05b24890c9f1b1d7cbd
Micha de Vries <contact@michadevries.nl>
2020-05-14 16:52:30 +0000

    Release v1.0.6: Bug fixes

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const app = new Rable({
 
 var count = 0;
 
-app.setProxy({
-	host: 'rable.app.michadevries.nl',
-	secure: true
-});
-
-app.info.set('name', 'Rable.JS Introduction');
+app.info.set('name', 'rable.js introduction');
 
 app.get('/', __dirname + '/views/home.html');
 app.get('/other', (req, res) => res.sendFile(__dirname + '/views/other.html'));
