92dc363a69f523bb5a4f09c6201c2c02ce80de0d
Micha de Vries <contact@michadevries.nl>
2020-07-08 16:24:51 +0000

    Release v1.1.0: First major release

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const Rable = require(__dirname + '/lib');
 const app = new Rable({
 	port: 3001,
-	assets: 'assets'
+	assets: 'assets',
+	root: 0
 });
 
-var count = 0;
+const secondary = new Rable({
+	secondary: true,
+	assets: 'assets',
+	root: 1
+});
 
 app.info.set('name', 'rable.js ' + app.version + ' introduction');
+secondary.info.set('name', 'Secondary router');
 
 app.get('/', __dirname + '/views/home.html');
 app.get('/other', (req, res) => res.sendFile(__dirname + '/views/other.html'));
@@ -15,3 +21,7 @@ app.get('/servererror', (req, res) => res.sendError(500, req.query));
 
 app.static('/assets', __dirname + '/assets');
 app.static('/docs', __dirname + '/docs/rable.js', {noTemplate: true});
+
+app.all('/secondary/~', function(req, res) { secondary.handle(req, res) });
+secondary.get('/', (req, res) => res.send('This is the root url of the secondary router.'));
+secondary.get('/other', (req, res) => res.send('This is the other page of the secondary router.'));
